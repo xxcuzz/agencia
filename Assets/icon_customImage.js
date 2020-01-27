0fbd105da230981dee85b6b7e2901bda7d456cc5
xxcuzz <xxcuzzx@gmail.com>
2020-01-27 22:59:39 +0300

    mapdark

--- Assets/icon_customImage.js
+++ Assets/icon_customImage.js
@@ -1,28 +1,44 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [51.681145514139736,-0.3096831896586125],
-            zoom: 11,
-            controls: [],
+ymaps.ready(function() {
+  var myMap = new ymaps.Map('map', {
+      center: [51.681145514139736, -0.3096831896586125],
+      zoom: 11,
+      controls: [],
 
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
-            iconLayout: 'default#image',
-            iconImageHref: 'Assets/images/map_circle.png',
-            iconImageSize: [34, 34],
-        });
-        myMap.behaviors.disable('drag');
-        myMap.behaviors.disable('scrollZoom');
-        var balloon = myMap.balloon.open(myMap.getCenter(), {
-          content: '<p id = "mem"> "9833 Martina Path"</p>',
-        }, {
-          closeButton: false,
-          offset: [3,-40],
-         })
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'Assets/images/map_circle.png',
+      iconImageSize: [34, 34],
+    });
 
-    myMap.geoObjects
-        .add(myPlacemark)
-        .add(myPlacemarkWithContent);
+    var myPolygon = new ymaps.Polygon([
+           [
+               [55.75, 37.50],
+               [55.75, -50.70],
+               [45.70, -50.70],
+               [45.70, 37.50]
+           ],
+       ], {
+       }, {
+           fillColor: '#000000',
+           opacity: 0.1
+       });
+
+
+  myMap.behaviors.disable('drag');
+  myMap.behaviors.disable('scrollZoom');
+  var balloon = myMap.balloon.open(myMap.getCenter(), {
+    content: '<p id = "mem"> "9833 Martina Path"</p>',
+  }, {
+    closeButton: false,
+    offset: [3, -40],
+  })
+
+  myMap.geoObjects
+    .add(myPolygon)
+    .add(myPlacemark)
+    .add(myPlacemarkWithContent);
 });
